29fe7fda57085c4fe831fa244e715a643efbaef5
Matteo Collina <hello@matteocollina.com>
2017-03-06 18:45:56 +0000

    http2: support delayed open scenario for createClientSession

---
+++ test/parallel/test-http2-create-client-session-delayed-open.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const common = require('../common');
+const assert = require('assert');
+const h2 = require('http2');
+const net = require('net');
+const body =
+  '<html><head></head><body><h1>this is some data</h2></body></html>';
+
+const server = h2.createServer();
+
+// we use the lower-level API here
+server.on('stream', common.mustCall(onStream));
+
+function onStream(stream) {
+  stream.respond({
+    'content-type': 'text/html',
+    ':status': 200
+  });
+  stream.end(body);
+}
+
+server.listen(0);
+
+server.on('listening', common.mustCall(function() {
+  const socket = net.connect(this.address());
+
+  const client = h2.createClientSession(socket);
+
+  const headers = {
+    ':method': 'GET',
+    ':scheme': 'http',
+    ':authority': `localhost:${this.address().port}`,
+    ':path': '/'
+  };
+
+  const req = client.request(headers);
+
+  req.on('response', common.mustCall(function(headers) {
+    assert.strictEqual(headers[':status'], '200', 'status code is set');
+    assert.strictEqual(headers['content-type'], 'text/html',
+                       'content type is set');
+    assert(headers['date'], 'there is a date');
+  }));
+
+  let data = '';
+  req.setEncoding('utf8');
+  req.on('data', (d) => data += d);
+  req.on('end', common.mustCall(function() {
+    assert.strictEqual(body, data);
+    server.close();
+    socket.destroy();
+  }));
+
+  req.end();
+}));
