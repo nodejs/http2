24c3779b25287ea030de00f7559688057fca4ee8
Robert Kowalski <rok@kowalski.gd>
2017-05-08 22:02:21 +0200

    http2: add test-http2-date-header.js

---
+++ test/parallel/test-http2-date-header.js
@@ -0,0 +1,38 @@
+'use strict';
+
+require('../common');
+const assert = require('assert');
+const http2 = require('http2');
+
+const testResBody = 'other stuff!\n';
+
+const server = http2.createServer((req, res) => {
+  assert.ok(!('date' in req.headers),
+            'Request headers did not contain a date.');
+  res.writeHead(200, {
+    'Content-Type': 'text/plain'
+  });
+  res.end(testResBody);
+});
+server.listen(0);
+
+server.on('listening', function() {
+  const client = http2.connect(`http://localhost:${this.address().port}`);
+
+  const headers = { ':path': '/' };
+  const req = client.request(headers).setEncoding('utf8');
+
+  req.on('response', (headers) => {
+    assert.ok('date' in headers,
+              'Response headers contain a date.');
+
+    req.resume();
+  });
+
+  req.on('end', () => {
+    server.close();
+    process.exit();
+  });
+
+  req.end();
+});
