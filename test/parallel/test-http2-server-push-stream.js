055dca1ff11988454b5c6f374bcc37f8b8ae7fd7
Yosuke Furukawa <yosuke.furukawa@gmail.com>
2017-05-10 01:10:19 +0900

    http2: add server push event for client
    PR-URL: https://github.com/nodejs/http2/pull/95
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: James M Snell <jasnell@gmail.com>

---
+++ test/parallel/test-http2-server-push-stream.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const common = require('../common');
+const assert = require('assert');
+const http2 = require('http2');
+
+const server = http2.createServer();
+server.on('stream', common.mustCall((stream, headers, flags) => {
+  const port = server.address().port;
+  if (headers[':path'] === '/') {
+    stream.pushStream({
+      ':scheme': 'http',
+      ':path': '/foobar',
+      ':authority': `localhost:${port}`,
+    }, (stream, headers) => {
+      stream.respond({
+        'content-type': 'text/html',
+        ':status': 200,
+        'x-push-data': 'pushed by server',
+      });
+      stream.end('pushed by server data');
+    });
+  }
+  stream.respond({
+    'content-type': 'text/html',
+    ':status': 200
+  });
+  stream.end('test');
+}));
+
+server.listen(0, common.mustCall(() => {
+  const port = server.address().port;
+  const headers = { ':path': '/' };
+  const client = http2.connect(`http://localhost:${port}`);
+  const req = client.request(headers);
+
+  client.on('push', common.mustCall((stream, headers, flag) => {
+    assert.strictEqual(headers[':status'], '200');
+    assert.strictEqual(headers['content-type'], 'text/html');
+    assert.strictEqual(headers['x-push-data'], 'pushed by server');
+  }));
+
+  let data = '';
+
+  req.on('data', common.mustCall((d) => data += d));
+  req.on('end', common.mustCall(() => {
+    assert.strictEqual(data, 'test');
+    server.close();
+    client.destroy();
+  }));
+  req.end();
+}));
