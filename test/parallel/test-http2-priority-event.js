9f4403e3c95465cf8972e7085097f832a1c03d5b
James M Snell <jasnell@gmail.com>
2017-05-14 07:55:22 -0700

    http2: add priority advisement event
    Trigger a 'priority' event on the session, stream, and server
    when a priority frame is received.
    
    Refs: https://github.com/nodejs/http2/issues/69
    
    PR-URL: https://github.com/nodejs/http2/pull/110
    Ref: https://github.com/nodejs/http2/issues/69
    Reviewed-By: Matteo Collina <matteo.collina@gmail.com>
    Reviewed-By: Yosuke Furukawa <yosuke.furukawa@gmail.com>

---
+++ test/parallel/test-http2-priority-event.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const common = require('../common');
+const assert = require('assert');
+const h2 = require('http2');
+
+const server = h2.createServer();
+
+// we use the lower-level API here
+server.on('stream', common.mustCall(onStream));
+
+function onPriority(stream, parent, weight, exclusive) {
+  assert.strictEqual(stream, 1);
+  assert.strictEqual(parent, 0);
+  assert.strictEqual(weight, 1);
+  assert.strictEqual(exclusive, false);
+}
+
+function onStream(stream, headers, flags) {
+  stream.priority({
+    parent: 0,
+    weight: 1,
+    exclusive: false
+  });
+  stream.respond({
+    'content-type': 'text/html',
+    ':status': 200
+  });
+  stream.end('hello world');
+}
+
+server.listen(0);
+
+server.on('priority', common.mustCall(onPriority));
+
+server.on('listening', common.mustCall(() => {
+
+  const client = h2.connect(`http://localhost:${server.address().port}`);
+
+  client.on('priority', common.mustCall(onPriority));
+
+  const req = client.request({ ':path': '/'});
+
+  client.on('connect', () => {
+    req.priority({
+      parent: 0,
+      weight: 1,
+      exclusive: false
+    });
+  });
+
+  req.on('response', common.mustCall());
+  req.on('data', common.noop);
+  req.on('end', common.mustCall(() => {
+    server.close();
+    client.destroy();
+  }));
+  req.end();
+
+}));
